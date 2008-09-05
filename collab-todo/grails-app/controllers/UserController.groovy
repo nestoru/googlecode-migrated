d6a0fd8549c7acd0da188166f2b1526776423a63
nestor.urquiza <nestor.urquiza@2d8599ef-2b56-0410-99ad-bd7dcdf96713>
2008-09-05 21:24:59 +0000

    First commit

---
+++ collab-todo/grails-app/controllers/UserController.groovy
@@ -0,0 +1,25 @@
+class UserController {
+
+    def scaffold 
+    {scaffold = User}
+    
+    def login = {}
+    
+    def handleLogin = {
+        
+	    def user = User.findByUserName(params.userName)
+        
+		if (!user) {
+			flash.message = "User not found for userName: ${params.userName}"
+			redirect(action:'login')
+		}
+		session.user = user
+		redirect(controller:'todo')
+	}
+	def logout = {
+		if(session.user) {
+			session.user = null
+			redirect(action:'login')
+		}
+	}
+}
---
+++ collab-todo/grails-app/views/common/_buddies.gsp
@@ -0,0 +1,4 @@
+<%@ taglib prefix="g" uri="/web-app/WEB-INF/tld/grails.tld" %>
+<div id="buddies">
+  <div class="title">Buddies</div>
+</div>
