e68e36bfc61bfa742bb38e386b8ce4ebf3e8342e
nestor.urquiza <nestor.urquiza@2d8599ef-2b56-0410-99ad-bd7dcdf96713>
2008-09-10 18:30:31 +0000

    REST Services working now

--- collab-todo/grails-app/conf/UrlMappings.groovy
+++ collab-todo/grails-app/conf/UrlMappings.groovy
@@ -1,10 +1,18 @@
 class UrlMappings {
     static mappings = {
-      "/$controller/$action?/$id?"{
-	      constraints {
-			 // apply constraints here
-		  }
-	  }
-	  "500"(view:'/error')
-	}
+          "/$rest/$realcontroller/$id?"{
+            controller = "rest"
+            action = [GET:"show", PUT:"create", POST:"update", DELETE:"delete"]
+            constraints {
+                rest(inList:["rest","json"])
+            } 
+         }
+            
+         "/$controller/$action?/$id?"{
+    	      constraints {
+    			 // apply constraints here
+    		  }
+    	  }         
+          "500"(view:'/error')      
+    }
 }
