77baf4f06a892a293cba765985e71fadf25f3c02
nestor.urquiza <nestor.urquiza@2d8599ef-2b56-0410-99ad-bd7dcdf96713>
2008-09-16 00:56:11 +0000

    First commit

---
+++ trunk/fasttutorial/src/springmvc/web/CarListController.java
@@ -0,0 +1,30 @@
+package springmvc.web;
+ 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+ 
+import springmvc.service.CarManager;
+ 
+public class CarListController implements Controller {
+  private CarManager carManager;
+  
+	public ModelAndView handleRequest(HttpServletRequest arg0,
+			HttpServletResponse arg1) throws Exception {		
+ 
+		ModelAndView modelAndView = new ModelAndView("carList");
+		modelAndView.addObject("carList", carManager.getCarList());
+ 
+		return modelAndView;
+	}
+	
+	public CarManager getCarManager() {
+		return carManager;
+	}
+ 
+	public void setCarManager(CarManager carManager) {
+		this.carManager = carManager;
+	}
+}
