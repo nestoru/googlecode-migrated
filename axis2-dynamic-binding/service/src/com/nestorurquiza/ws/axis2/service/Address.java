6139e1d5d00a506d339d9860f4672624e1a5ced6
nestor.urquiza <nestor.urquiza@2d8599ef-2b56-0410-99ad-bd7dcdf96713>
2009-03-01 22:06:08 +0000

    First Import

---
+++ axis2-dynamic-binding/service/src/com/nestorurquiza/ws/axis2/service/Address.java
@@ -0,0 +1,22 @@
+package com.nestorurquiza.ws.axis2.service;
+
+public class Address {
+
+  private String country;
+  private String zip;
+  
+  public String getCountry(){
+    return country;
+  }
+  public void setCountry(String country){
+    this.country = country;
+  }
+  
+  public String getZip(){
+    return zip;
+  }
+  public void setZip(String zip){
+    this.zip = zip;
+  }
+  
+}
---
+++ axis2-dynamic-binding/service/src/com/nestorurquiza/ws/axis2/service/DynamicServiceSample.java
@@ -0,0 +1,53 @@
+package com.nestorurquiza.ws.axis2.service;
+
+public class DynamicServiceSample {
+  static private final long serialVersionUID = 2L;
+
+  public Person getPerson( String userName ) {
+    Person person = new Person();
+    person.setFirstName(getFirstName(userName));
+    person.setLastName(getLastName(userName));
+    Address address = new Address();
+    address.setCountry("USA");
+    address.setZip("44167");
+    person.setAddress(address);
+    return person;
+  }
+  
+  private String getFirstName( String userName ) {
+    String firstName = "unknown";
+    System.out.println("Serving getFirstName(\"" + userName + "\")");
+    if(!isEmpty(userName)) {
+      firstName = userName.split(" ")[0];
+    }
+    return firstName;
+  }
+ 
+  private String getLastName( String userName ) {
+    String lastName = "unknown";
+    System.out.println("Serving getLastName(\"" + userName + "\")");
+    if(!isEmpty(userName)) {
+      String[] tokens = userName.split(" ");
+      int length = tokens.length;      
+      if( length > 0 ) {
+        if( length > 1 ) {
+          if( length > 2 ) {
+            lastName = tokens[2];
+          }else{
+            lastName = tokens[1];
+          }
+        }else{
+          //Just firstName provided
+        }
+      }
+    }
+    return lastName;
+  }
+  
+  private boolean isEmpty(String myString){
+    if(myString==null || myString.trim().length()==0)
+      return true;
+    else
+      return false;
+  }
+}
