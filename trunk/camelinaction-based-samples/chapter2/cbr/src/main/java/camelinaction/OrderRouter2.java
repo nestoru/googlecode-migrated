bfe9a9f337d58366e4ce74ac5846f1c7c4448a91
nestor.urquiza@gmail.com <nestor.urquiza@gmail.com@2d8599ef-2b56-0410-99ad-bd7dcdf96713>
2012-03-22 16:40:59 +0000

    importing a modified version that uses http://code.google.com/p/rmannibucau/wiki/CamelDiagramGenerator plugin

---
+++ trunk/camelinaction-based-samples/chapter2/cbr/src/main/java/camelinaction/OrderRouter2.java
@@ -0,0 +1,57 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package camelinaction;
+
+import javax.jms.ConnectionFactory;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+
+/**
+ * A modified version from OrderRoute which uses an outer OrderRouteBuilder class
+ *
+ * @author nestor.urquiza
+ *
+ */
+public class OrderRouter2 {
+
+    public static void main(String args[]) throws Exception {
+        // create CamelContext
+        CamelContext context = new DefaultCamelContext();
+        
+        // connect to embedded ActiveMQ JMS broker
+        ConnectionFactory connectionFactory = 
+            new ActiveMQConnectionFactory("vm://localhost");
+        context.addComponent("jms",
+            JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+
+        // add our route to the CamelContext
+        context.addRoutes(new OrderRouteBuilder());
+
+        // start the route and let it do its work
+        context.start();
+        Thread.sleep(10000);
+
+        // stop the CamelContext
+        context.stop();
+    }
+}
