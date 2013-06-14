6fdc997d007001e3d2953c302ca0b69c080d96b4
nestor.urquiza@gmail.com <nestor.urquiza@gmail.com@2d8599ef-2b56-0410-99ad-bd7dcdf96713>
2013-06-14 17:50:42 +0000

    v 1.3

--- chrome-plugins/remove-cookies-for-site/cookie_handler.js
+++ chrome-plugins/remove-cookies-for-site/cookie_handler.js
@@ -1,5 +1,52 @@
-chrome.extension.onRequest.addListener(
-  function(request, sender, sendResponse) {
-	javascript:(function(){C=document.cookie.split("; ");for(d="."+location.host;d;d=(""+d).substr(1).match(/\..*$/))for(sl=0;sl<2;++sl)for(p="/"+location.pathname;p;p=p.substring(0,p.lastIndexOf('/')))for(i in C)if(c=C[i]){document.cookie=c+"; domain="+d.slice(sl)+"; path="+p.slice(1)+"/"+"; expires="+new Date((new Date).getTime()-1e11).toGMTString()}})();  
-  }
-);
+this.cookies_ = {};
+
+function addCookie(cookie) {
+   	var key = cookie.name+cookie.domain+cookie.hostOnly+cookie.path+cookie.secure+cookie.httpOnly+cookie.session+cookie.storeId;
+	console.log(key);
+  	this.cookies_[key] = cookie;	
+}
+
+function listener(info) {
+	var cookie = info.cookie;
+	addCookie(cookie);
+}
+
+/*
+function onload() {
+	var foo = true;
+}
+*/
+
+jQuery(document).ready(function(){
+  	console.log("From cookie_handler.js:");
+    if (!chrome.cookies) {
+	  chrome.cookies = chrome.experimental.cookies;
+	}
+  	chrome.browserAction.onClicked.addListener(function(tab) {
+		chrome.cookies.getAll({}, function(cookies) {
+			chrome.cookies.onChanged.addListener(listener);
+			for( var i in cookies ) {
+				addCookie(cookies[i]);
+			}
+		});
+		setTimeout(runProcess(tab), 250);
+		//runProcess(tab);
+
+
+	});
+});
+
+function runProcess(tab) {	
+	var domain = extractDomain(tab.url);
+	for (var i in this.cookies_) {
+	   	var cookie = this.cookies_[i];
+		if( ("."+domain).indexOf(cookie.domain) != -1 ) {
+			  url = "http" + (cookie.secure ? "s" : "") + "://" + cookie.domain + cookie.path;
+			  chrome.cookies.remove({"url": url, "name": cookie.name});
+		}
+    }
+}
+
+function extractDomain(url) {
+	return url.match(/:\/\/(.[^/:]+)/)[1];
+}
